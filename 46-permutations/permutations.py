66aad191c505c93d8f1396fb6f6f965dcb604ef8
shravax <sharavannn456@gmail.com>
2026-08-09 22:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (34.09%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,19 @@
+# LeetCode 46. Permutations
+# Time: O(n * n!) | Space: O(n)
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def backtrack(start):
+            if start == n:
+                res.append(nums[:])
+                return
+            for i in range(start, n):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+        
+        backtrack(0)
+        return res
